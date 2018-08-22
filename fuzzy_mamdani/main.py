e8a8ef8021214c3a94a40b4ec8d1e5bb31175e1d
tanishqjasoria <jasoriatanishq@gmail.com>
2018-08-22 06:42:48 +0530

    errors

--- fuzzy_mamdani/main.py
+++ fuzzy_mamdani/main.py
@@ -105,49 +105,52 @@ def rule_base(i_ltern,in_indext,li,lo,linguisticinput1,linguisticinput,linguisti
                     index_output_linguistic[0][ii_index]=i_lter
 
 def main():
+    no_output = 1
+    no_input = 2
     for dimension in range(no_input):
-        for i_lter in range(linguistic_input[dimension]):
+        for i_lter in range(li[dimension]):
             print("%lf\t\t%lf\n",linguistic_range_input_min[dimension][i_lter],linguistic_range_input_max[dimension][i_lter])
         print("\n")
     print("\n")
-    for dimension in range(no_output):
-        for i_lter in range(linguistic_output[dimension]):
-            print("%lf\t\t%lf\n",linguistic_range_output_min[dimension][i_lter],linguistic_range_output_max[dimension][i_lter])
-        print("\n")
-    print("\n")
-    data_base(ii,linguistic_number,linguistic_input,no_input,i_inp,linguistic_range_input_min,linguistic_range_input_max,linguistic_range_output_min,linguistic_range_output_max)
+    #for dimension in range(no_output):
+    #    for i_lter in range(lo[dimension]):
+    #        print(linguistic_range_output_min[dimension][i_lter])
+    #        print("   ")
+    #        print(linguistic_range_output_max[dimension][i_lter]))
+    ii=[0]*5000
+    linguistic_number = [[0]*100]*5000
+    data_base(ii,linguistic_number,li,no_input,i_inp,linguistic_range_input_min,linguistic_range_input_max,linguistic_range_output_min,linguistic_range_output_max)
     for dimension in range(no_input):
         for iii in range(ii[dimension]):
-            print("%lf\t",linguisticinput[dimension][linguistic_number[dimension][iii]])
+            print(linguisticinput[dimension][linguistic_number[dimension][iii]])
     for dimension in range(no_input):
         for iii in range(ii[dimension]):
             linguisticinput1[dimension][linguistic_number[dimension][iii]]=linguisticinput[dimension][linguistic_number[dimension][iii]]
-    rule_base(i_lterni,in_indext,linguistic_input,linguistic_output,linguisticinput1,linguisticinput,linguisticoutput1,linguisticoutput_if_else,ii,linguistic_number,i_lternt,linguisticoutput,index_output_linguistic,no_output)
-    print("\n%d",i_lterni)
-    for ii_index in i_lterni:
-        print("\n%lf",linguisticoutput1[0][ii_index])
+    rule_base(i_lterni,in_indext,li,lo,linguisticinput1,linguisticinput,linguisticoutput1,linguisticoutput_if_else,ii,linguistic_number,i_lternt,linguisticoutput,index_output_linguistic,no_output)
+    print(i_lterni)
+    print(linguisticoutput1[0])
 
-    membership_function(membership_function_value_input,ii,linguistic_number,linguistic_input,no_input,i_inp,linguistic_range_input_min,linguistic_range_input_max,linguistic_range_output_min,linguistic_range_output_max)
+    membership_function(membership_function_value_input,ii,linguistic_number,li,no_input,i_inp,linguistic_range_input_min,linguistic_range_input_max,linguistic_range_output_min,linguistic_range_output_max)
     for dimension in range(no_input):
         for iii in range(ii[dimension]):
-            print("\n%lf\t",membership_function_value_input[dimension][linguistic_number[dimension][iii]])
+            print(membership_function_value_input[dimension][linguistic_number[dimension][iii]])
     iii_index = 0;
     for dimension in range(no_input):
         for iii in range(ii[dimension]):
             for dimension1 in range(no_input):
                 alpha[0][iii_index]=min(membership_function_value_input[dimension][linguistic_number[dimension][iii]],membership_function_value_input[dimension1][linguistic_number[dimension1][jjj]])
-                print("\n%lf",alpha[0][iii_index])
+                print(alpha[0][iii_index])
                 iii_index=iii_index+1
-    area_under_curve(index_output_linguistic,i_lterni,area,alpha,start,end,linguistic_output,no_output,i_inp,linguistic_range_input_min,linguistic_range_input_max,linguistic_range_output_min,linguistic_range_output_max)
+    area_under_curve(index_output_linguistic,i_lterni,area,alpha,start,end,lo,no_output,i_inp,linguistic_range_input_min,linguistic_range_input_max,linguistic_range_output_min,linguistic_range_output_max)
     #printf("\n");
 
     for dimension in range(no_output):
         for iii in range(i_lterni):
-            print("\n%lf",area[dimension][iii])
-    centroid_under_curve(index_output_linguistic,i_lterni,centroid,alpha,start,end,linguistic_output,no_output,i_inp,linguistic_range_input_min,linguistic_range_input_max,linguistic_range_output_min,linguistic_range_output_max)
+            print(area[dimension][iii])
+    centroid_under_curve(index_output_linguistic,i_lterni,centroid,alpha,start,end,lo,no_output,i_inp,linguistic_range_input_min,linguistic_range_input_max,linguistic_range_output_min,linguistic_range_output_max)
     for dimension in range(no_output):
         for iii in range(i_lterni):
-            print("\n%lf",centroid[dimension][iii])
+            print(centroid[dimension][iii])
     for dimension in range(no_output):
         LCM[dimension]=0
         HCF[dimension]=0
@@ -155,4 +158,6 @@ def main():
             LCM[dimension]=LCM[dimension]+area[dimension][iii]*centroid[dimension][iii]
             HCF[dimension]=HCF[dimension]+area[dimension][iii]
             crisp_output[dimension]=LCM[dimension]/HCF[dimension]
-        print("\n%lf",crisp_output[dimension])
+        print(crisp_output[dimension])
+
+main()
