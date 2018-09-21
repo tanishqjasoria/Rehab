1d40e2de7b1abe4faba7c04be3f85fbcf9f2070c
Tanishq Jasoria <jasoriatanishq@gmail.com>
2018-09-21 17:05:37 +0530

    push

--- gaitCycle-mimickSetup/relayTuning_Roboclaw/indv_2/position_init.py
+++ gaitCycle-mimickSetup/relayTuning_Roboclaw/indv_2/position_init.py
@@ -9,7 +9,7 @@ from roboclaw import Roboclaw
 #Windows comport name
 #rc = Roboclaw("COM3",115200)
 #Linux comport name
-rc = Roboclaw("/dev/ttyACM0",115200)
+rc = Roboclaw("/dev/ttyACM1",115200)
 f=open("final.csv.txt","w+")
 
 def displayspeed(ti):
